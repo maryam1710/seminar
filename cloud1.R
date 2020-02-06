aaf9451d13aba8dfe605e3564698c907b793dd13
maryam1710 <57703353+maryam1710@users.noreply.github.com>
2020-02-06 12:26:35 +0330

    نمودار پراکندگی سه بعدی
    با استفاده از داده‌های iris

--- cloud1.R
+++ cloud1.R
@@ -1,2 +1,2 @@
 cloud(Sepal.Length ~ Petal.Length * Petal.Width |Species  , data = iris,
-      screen = list(x = -90, y = 70),distance = .4, zoom = .6)
+      screen = list(x = -90, y = 70),distance = .4, zoom = .5)
